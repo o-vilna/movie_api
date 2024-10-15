adb61f378b72df054e94f70e19951af276168de1
o-vilna <162020673+o-vilna@users.noreply.github.com>
2024-10-15 17:01:44 +0200

    Updated password hashing logic

--- index.js
+++ index.js
@@ -227,6 +227,8 @@ app.post(
       return res.status(422).json({ errors: errors.array() });
     }
     let hashedPassword = Users.hashPassword(req.body.Password);
+
+    console.log("Hashed password:", hashedPassword);
     await Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
